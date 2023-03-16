439759ee25f0510333bc59813ddc59f1f7d69547
Steven Wickers <stevenwickers@gmail.com>
2023-03-16 12:00:13 -0400

    udpated the rollup config to output jestconfig file to bundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import replace from '@rollup/plugin-replace'
 import typescript from '@rollup/plugin-typescript'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import resolve from '@rollup/plugin-node-resolve';
-export default {
+export default [{
   input: 'src/index.ts',
   output: {
     file: 'dist/bundle.js',
@@ -24,4 +24,27 @@ export default {
     nodeResolve(),
     resolve()
   ],
-}
+},
+  {
+    input: 'jestconfig.js',
+    output: {
+      file: 'dist/bundle.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: [
+      typescript({compilerOptions: {lib: ["es5", "es6", "dom"], target: "es5"}}),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('development'),
+        preventAssignment: true,
+      }),
+      babel({
+        presets: ['@babel/preset-react'],
+        babelHelpers: 'bundled'
+      }),
+      typescript(),
+      nodeResolve(),
+      resolve()
+    ],
+  }
+  ]
